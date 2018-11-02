659811e5632b1012a0ca8ed8f7b0cb0fead57270
PC_G4600 <Armen.181@gmail.com>
2018-11-02 07:44:42 +0400

    Initial commit

---
+++ src/main/java/net/ddns/armen181/springmvc/SpringmvcApplication.java
@@ -0,0 +1,12 @@
+package net.ddns.armen181.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcApplication.class, args);
+    }
+}
---
+++ src/test/java/net/ddns/armen181/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ddns.armen181.springmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
